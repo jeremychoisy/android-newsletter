7688a2929570db5bbb9aaf0c2709d78b2b9fde16
Panz <alexpanz06@gmail.com>
2020-11-09 16:26:42 +0100

    init home

--- app/src/main/java/com/mbds/newsletter/fragments/HomeFragment.kt
+++ app/src/main/java/com/mbds/newsletter/fragments/HomeFragment.kt
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import com.google.android.material.tabs.TabLayout
 import com.mbds.newsletter.MainActivity
 import com.mbds.newsletter.R
 
@@ -23,9 +24,25 @@ class HomeFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        val button = view.findViewById<Button>(R.id.button)
-        button.setOnClickListener {
-            (activity as? MainActivity)?.changeFragment(CategoriesFragment())
-        }
+//        view.findViewById<TabLayout>(R.id.tabLayout).tabMode = TabLayout.MODE_SCROLLABLE
+        view.findViewById<TabLayout>(R.id.tabLayout).addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener {
+
+            override fun onTabSelected(tab: TabLayout.Tab?) {
+                // Handle tab select
+                println(tab.toString())
+            }
+
+            override fun onTabReselected(tab: TabLayout.Tab?) {
+                // Handle tab reselect
+            }
+
+            override fun onTabUnselected(tab: TabLayout.Tab?) {
+                // Handle tab unselect
+            }
+        })
+//        val button = view.findViewById<Button>(R.id.button)
+//        button.setOnClickListener {
+//            (activity as? MainActivity)?.changeFragment(CategoriesFragment())
+//        }
     }
 }
